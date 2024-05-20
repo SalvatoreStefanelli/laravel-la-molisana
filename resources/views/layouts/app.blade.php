daa4a524bf1df9162ea00e86693c2a4f2596b5e2
SalvatoreStefanelli <stefanellisalvatore.ss@gmail.com>
2024-05-20 23:20:40 +0200

    add page footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,21 +16,14 @@
         
     </head>
     <body class="antialiased">
-
-        <header>        
-            <img src="{{ Vite::asset('resources/images/la-molisana-logo.png') }}">
-            <nav>
-                <a href="/">Home</a>
-                <a href="/products">Products</a>
-                <a href="/news">News</a>
-            </nav>
-        </header>
+        @include('partials/header')       
 
         <main>
 
             @yield('content')
 
         </main>
-        <footer>footer</footer>
+        @include('partials/footer')       
+        
     </body>
 </html>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,8 @@
+<header>        
+        <img src="{{ Vite::asset('resources/images/la-molisana-logo.png') }}">
+        <nav>
+            <a href="/">Home</a>
+            <a href="/products">Products</a>
+            <a href="/news">News</a>
+        </nav>
+</header>
